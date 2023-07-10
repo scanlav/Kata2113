b68a7f7d91bd8633798ebe2a3fcc843bed2daaf5
Viktor Churikov <scanlave@gmail.com>
2023-07-10 22:42:46 +0300

    Исправления по замечаниям.

--- src/test/java/TipServiceTest.java
+++ src/test/java/TipServiceTest.java
@@ -1,20 +1,15 @@
 import org.example.TipService;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TipServiceTest {
     TipService tipService = new TipService();
 
-    @DisplayName("Граничные значения")
-    @ParameterizedTest()
+    @ParameterizedTest(name = "Граничные значения")
     @CsvSource({"-1", "0", "1", "499", "500", "501", "999", "1000", "1001"})
     void roundTipTest(BigDecimal num) {
         BigDecimal expected;
@@ -27,31 +22,5 @@ public class TipServiceTest {
 
         assertEquals(expected, tipService.roundTips(num));
     }
-
-    @DisplayName("Выброс исключения при передаче в метод значения Null")
-    @ParameterizedTest
-    @NullSource
-    void roundTipNullTest(BigDecimal num) {
-        assertThrows(NullPointerException.class, () -> tipService.roundTips(num));
-    }
-
-
-    @DisplayName("Выброс исключения при передачи строки вместо BigDecimal")
-    @ParameterizedTest
-    @ValueSource(strings = {"str"})
-    void roundTipStringTest(String str) {
-        assertThrows(NumberFormatException.class, () -> tipService.roundTips(new BigDecimal(str)));
-    }
-
-
-    @DisplayName("Конвертирование спецсимволов в BigDecimal")
-    @ParameterizedTest
-    @CsvSource({"$", ";", "#", "&", "("})
-    void roundTipCharTest(Character character) {
-        BigDecimal result = tipService.roundTips(new BigDecimal(character));
-        assertTrue(result instanceof BigDecimal);
-    }
-
-
 }
 
